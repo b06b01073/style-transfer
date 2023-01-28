654cd6d20bf8c0d026d9dbcff0069a9a67534255
b06b01073 <b06b01073@gmail.com>
2023-01-28 23:09:14 +0800

    can now get the gram matrix of style representation

--- main.py
+++ main.py
@@ -1,24 +1,44 @@
-from vgg19 import VGG19
-from PIL import Image
 from torchvision import transforms
 from torchvision.utils import save_image
+import torch
 
+from PIL import Image
+from collections import defaultdict
 import os
 
-def main():
+from vgg19 import VGG19
 
+def main():
     model = VGG19()
 
+    with torch.no_grad():
+        content_representation, style_representation = get_representations(model)
+
+    
+    
+
+def get_representations(model, visualize=False):
+    '''precompute the data of Gram matrix in style representation and the content representation 
+    '''
     content_img = read_img('images/content_images/cat.jpg')
     content_img_tensor = transform_img(content_img)
 
     content_representation = model(content_img_tensor)
-    visualize_filter(content_representation, 'images/content_representation')
+
 
     style_img = read_img('images/style_images/water_painting.jpg')
     style_img_tensor = transform_img(style_img)
     style_representation = model(style_img_tensor)
-    visualize_filter(style_representation, 'images/style_representation')
+    style_representation = flatten_representation(style_representation)
+    style_representation = get_gram_matrices(style_representation)
+
+    if visualize:
+        visualize_filter(style_representation, 'images/style_representation')
+
+    content_representation = flatten_representation(content_representation)
+
+    return content_representation, style_representation
+    
 
 def read_img(img_path):
     dir_path = os.path.dirname(__file__) 
@@ -35,13 +55,20 @@ def transform_img(img):
 
     return transformer(img)
 
-def normalize_img(img):
-    return img / 255
-
 def visualize_filter(representation, path):
     for k, v in representation.items():
-        save_image(v[0].unsqueeze(0), os.path.join(path, f'{k}.png'))
+        save_image(v.unsqueeze(0), os.path.join(path, f'{k}.png'))
 
+def get_gram_matrices(style_representations):
+    for layer_name, filter in style_representations.items():
+        filter_trans = torch.transpose(filter, 0, 1)
+        style_representations[layer_name] = torch.matmul(filter, filter_trans)
+    return style_representations
+
+def flatten_representation(representation):
+    for layer_name, filter in representation.items():
+        representation[layer_name] = torch.flatten(filter, start_dim=1)
+    return representation
 
 if __name__ == '__main__':
     main()
